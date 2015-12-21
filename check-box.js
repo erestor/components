ebe60e9c8ee61113b256beceef898430406dc414
Martin Klemsa <m.klemsa@gmail.com>
2015-12-21 17:17:21 +0100

    added check-box component

---
+++ check-box.js
@@ -0,0 +1,15 @@
+﻿define(['text!./check-box.html'],
+	function(htmlString) {
+
+	var ViewModel = function(params) {
+		this.label = params.label;
+		this.value = params.value;
+		this.title = params.title;
+		this.enable = params.enable !== undefined ? params.enable : true;
+	};
+
+    return {
+		'viewModel': ViewModel,
+		'template': htmlString
+	};
+});
