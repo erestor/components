72773681075375483c1a5c4a256d89eea52949ac
Martin Klemsa <m.klemsa@gmail.com>
2017-07-25 16:51:00 +0200

    Added toggle-button component
    - This is a wrapper for paper-toggle-button usable with KnockoutJS

---
+++ toggle-button/toggle-button.js
@@ -0,0 +1,22 @@
+﻿define(['text!./toggle-button.html', '../tools/tools'],
+function(htmlString, tools) {
+
+	var ViewModel = function(params) {
+		this.label = params.label;
+		this.value = params.value;
+		this.title = params.title;
+		this.enable = params.enable !== undefined ? params.enable : true;
+		this.dialogId = tools.getUuid();
+	};
+	ViewModel.prototype = {
+		'onChanged': function() {
+			var d = $('#' + this.dialogId)[0];
+			this.value(d.checked);
+		}
+	};
+
+    return {
+		'viewModel': ViewModel,
+		'template': htmlString
+	};
+});
