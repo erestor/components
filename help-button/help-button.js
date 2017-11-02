9cba594dc339a907dfe3af7c899f8488c1c50b3d
Martin Klemsa <m.klemsa@gmail.com>
2017-11-02 10:26:30 +0100

    Made help dialog always a child of body
    - To prevent stacking issues

--- help-button/help-button.js
+++ help-button/help-button.js
@@ -6,20 +6,28 @@
 		this.title = params.title;
 		this.data = params.data; //for templated version
 		this.noPosition = params.noPosition;
-		this.dialogId = tools.getGuid();
 		this.icon = params.outline ? 'icons:help-outline' : 'icons:help';
+		this.dialogId = tools.getGuid();
+		this.dialogEl = null;
 	};
 	ViewModel.prototype = {
 		'open': function() {
-			var d = $('#' + this.dialogId)[0];
+			if (!this.dialogEl)
+				this.dialogEl = $('#' + this.dialogId).appendTo($('body')); //must put the dialog in the body to ensure proper stacking
+
+			var d = this.dialogEl[0];
 			if (d.__isAnimating) {
 				//this is an unfortunate hack to prevent re-opening the dialog on click while it's closing as a result of polymer handler
 				return;
 			}
 			if (!this.noPosition)
-				d.positionTarget = $('.' + this.dialogId)[0];
+				d.positionTarget = $('.' + this.dialogId)[0]; //looking for a class(!)
 
 			d.open();
+		},
+		'dispose': function() {
+			if (this.dialogEl)
+				this.dialogEl.remove();
 		}
 	};
     return {
