f65bd4441f5cb205642ea948fb38c5a32da0d1d4
Martin Klemsa <m.klemsa@gmail.com>
2017-11-02 09:17:39 +0100

    Added progress-bar component
    - This is a knockout adapter for paper-progress

---
+++ progress-bar/progress-bar.js
@@ -0,0 +1,17 @@
+﻿define(['text!./progress-bar.html'],
+function(htmlString) {
+
+	var ViewModel = function(params) {
+		this.indeterminate = params.indeterminate;
+		this.min = params.min || 0;
+		this.max = params.max || 100;
+		this.secondaryProgress = params.secondaryProgress || 0;
+		this.value = params.value;
+		this.enable = params.enable !== undefined ? params.enable : true;
+	};
+
+    return {
+		'viewModel': ViewModel,
+		'template': htmlString
+	};
+});
