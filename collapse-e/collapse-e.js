dc5748fc91c1e6f0b85535e7d0fa9d28bcfd4c05
Martin Klemsa <m.klemsa@gmail.com>
2017-12-04 09:23:22 +0100

    Added collapse-r and collapse-e components
    - These allow implementing customized collapsible content

---
+++ collapse-e/collapse-e.js
@@ -0,0 +1,11 @@
+﻿define(['text!./collapse-e.html'], function(htmlString) {
+	var ViewModel = function(params) {
+		this.data = params.data;
+		this.opened = params.opened;
+	};
+
+    return {
+		'viewModel': ViewModel,
+		'template': htmlString
+	};
+});
---
+++ collapse-r/collapse-r.js
@@ -0,0 +1,16 @@
+﻿define(['text!./collapse-r.html'], function(htmlString) {
+	var ViewModel = function(params) {
+		this.data = params.data;
+		this.opened = params.opened;
+	};
+	ViewModel.prototype = {
+		'click': function() {
+			this.opened(!this.opened());
+		}
+	};
+
+    return {
+		'viewModel': ViewModel,
+		'template': htmlString
+	};
+});
