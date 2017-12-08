d69dbf2294af804f303346b62b89c003f10c4254
Martin Klemsa <m.klemsa@gmail.com>
2017-12-08 15:28:07 +0100

    Added checkbox-x as a knockout wrapper for paper-checkbox

---
+++ checkbox-x/checkbox-x.js
@@ -0,0 +1,22 @@
+﻿define(['text!./checkbox-x.html', '../tools/tools'],
+function(htmlString, tools) {
+
+	var ViewModel = function(params) {
+		this.checked = params.checked;
+		this.label = params.label;
+		this.noLabel = params.label === undefined;
+		this.disable = params.disable !== undefined ? params.disable : false;
+		this.id = tools.getGuid();
+	};
+	ViewModel.prototype = {
+		'onChanged': function() {
+			var d = $('#' + this.id)[0];
+			this.checked(d.checked);
+		}
+	};
+
+    return {
+		'viewModel': ViewModel,
+		'template': htmlString
+	};
+});
