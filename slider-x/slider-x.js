e8b429c40c588890f543dc50b8643c07a6016a4b
Martin Klemsa <m.klemsa@gmail.com>
2018-01-05 09:57:26 +0100

    Added marker capability to slider-x

--- slider-x/slider-x.js
+++ slider-x/slider-x.js
@@ -1,14 +1,27 @@
 ﻿define(['text!./slider-x.html', '../tools/tools'],
 function(htmlString, tools) {
 
-	var ViewModel = function(params) {
-		this.min = params.min || 0;
-		this.max = params.max || 100;
-		this.secondaryProgress = params.secondaryProgress || 0;
-		this.value = params.value;
-		this.pin = params.pin;
-		this.enable = params.enable !== undefined ? params.enable : true;
+	var ViewModel = function (params) {
+		//the order of the attributes matter - especially the value must be after min and max
 		this.dialogId = tools.getGuid();
+		this.value = params.value;
+		this.sliderAttrs = {
+			'id': this.dialogId,
+			'min': params.min || 0,
+			'max': params.max || 100,
+			'value': this.value,
+			'pin': params.pin
+		};
+		if (params.secondaryProgress)
+			this.sliderAttrs['secondary-progress'] = params.secondaryProgress;
+
+		if (params.marked) {
+			this.sliderAttrs.snaps = true;
+			this.sliderAttrs.step = 1;
+			this.sliderAttrs['max-markers'] = this.sliderAttrs.max;
+		}
+
+		this.enable = params.enable !== undefined ? params.enable : true;
 	};
 	ViewModel.prototype = {
 		'onChanged': function() {
