d93ef9eaa32f278c1dd9c4269a8e7e7c6a2318fc
Martin Klemsa <m.klemsa@gmail.com>
2019-02-14 17:27:50 +0100

    Added material-banner component

---
+++ material-banner/material-banner.js
@@ -0,0 +1,36 @@
+﻿define(['text!./material-banner.html', '../tools/tools'],
+function(htmlString, tools) {
+
+	var ViewModel = function(params) {
+		this.text = params.text;
+		this.buttons = ko.unwrap(params.buttons);
+		this.id = tools.getGuid();
+
+		var self = this;
+		var onButtonClick = function(index) {
+			self._hide();
+			if (self.buttons[index].click)
+				self.buttons[index].click();
+		};
+
+		this.onButton0 = function() {
+			onButtonClick(0);
+		};
+		this.onButton1 = function() {
+			onButtonClick(1);
+		};
+	};
+	ViewModel.prototype = {
+		'_hide': function() {
+			$('#' + this.id).slideUp({
+				duration: 375,
+				easing: 'easeOutCubic'
+			});
+		}
+	};
+
+    return {
+		'viewModel': ViewModel,
+		'template': htmlString
+	};
+});
