8a61bdc8e7fb661cef253601bb8e8ac2e2a5fef6
Martin Klemsa <m.klemsa@gmail.com>
2019-03-25 11:37:51 +0100

    Added 'clicked' param to material-checkbox

--- material-checkbox/material-checkbox.js
+++ material-checkbox/material-checkbox.js
@@ -3,6 +3,7 @@ function(htmlString, tools) {
 
 	var ViewModel = function(params) {
 		this.checked = params.checked;
+		this.clicked = params.clicked;
 		this.label = params.label;
 		this.noLabel = params.label === undefined;
 		this.noink = params.noink;
@@ -13,6 +14,10 @@ function(htmlString, tools) {
 		'onChanged': function() {
 			var d = $('#' + this.id)[0];
 			this.checked(d.checked);
+		},
+		'onTapped': function(vm, ev) {
+			if (this.clicked)
+				this.clicked(this.checked, ev);
 		}
 	};
 
