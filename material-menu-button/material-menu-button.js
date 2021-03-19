91b217cfd0ef314a61ee39ac8499905f1a679155
Martin Klemsa <m.klemsa@gmail.com>
2021-03-19 16:21:28 +0100

    Added material-menu-button component

---
+++ material-menu-button/material-menu-button.js
@@ -0,0 +1,34 @@
+﻿define(['text!./material-menu-button.html', '../material-select/material-select'],
+function(htmlString, materialSelectComponent) {
+
+	var MaterialSelectVM = materialSelectComponent.viewModel;
+
+	function ViewModel(params) {
+		MaterialSelectVM.call(this, params);
+		this.buttonCaption = params.buttonCaption;
+		this.noselect = params.noselect;
+		this.icon = params.icon || 'icons:arrow-drop-down';
+		this.fitInto = params.fitInto;
+	}
+	ViewModel.prototype = Object.create(MaterialSelectVM.prototype);
+	ViewModel.prototype.constructor = ViewModel;
+
+	ViewModel.prototype.openedChanged = function() {
+		if (!this.rootEl) {
+			this.rootEl = $('#' + this.rootId);
+			var m = this.rootEl[0],
+				d = m.$.dropdown,
+				containerEl = $(this.fitInto);
+
+			if (containerEl.length == 1)
+				d.fitInto = containerEl[0];
+
+			d.style.margin = "4px";
+		}
+	};
+
+    return {
+		'viewModel': ViewModel,
+		'template': htmlString
+	};
+});
