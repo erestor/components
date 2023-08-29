23147b552aa06bc7c25690541df9026bd6706df0
Martin Klemsa <m.klemsa@gmail.com>
2023-08-29 16:20:21 +0200

    Added material-button

---
+++ material-button/material-button.js
@@ -0,0 +1,23 @@
+﻿define(['text!./material-button.html', '../tools/tools'],
+function(htmlString, tools) {
+
+	var MaterialButton = function(params) {
+		//attributes
+		this.autofocus = params.autofocus;
+		this.dialogConfirm = params.dialogConfirm;
+		this.dialogDismiss = params.dialogDismiss;
+		this.slot = params.slot;
+		//this.id = tools.getGuid();
+
+		//data binding
+		this.click = params.click;
+		this.enable = tools.readEnableStatus(params);
+	};
+	MaterialButton.prototype = {
+	};
+
+    return {
+		'viewModel': MaterialButton,
+		'template': htmlString
+	};
+});
