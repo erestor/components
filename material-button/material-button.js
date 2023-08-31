716155e4337eb6deb21df948773a7d89a5f44666
Martin Klemsa <m.klemsa@gmail.com>
2023-08-31 15:25:48 +0200

    Switched to using mdc-button

--- material-button/material-button.js
+++ material-button/material-button.js
@@ -1,11 +1,12 @@
-﻿define(['text!./material-button.html', '../tools/tools'],
-function(htmlString, tools) {
+﻿define(['text!./material-button.html', '../tools/tools', '@material/ripple'],
+function(htmlString, tools, materialRipple) {
 
 	var MaterialButton = function(params) {
 		//attributes
 		this.autofocus = params.autofocus;
 		this.elevation = params.elevation;
 		this.noink = params.noink;
+		this.outlined = params.outlined;
 		this.raised = params.raised;
 
 		//data binding
@@ -16,7 +17,16 @@ function(htmlString, tools) {
 	};
 
 	return {
-		'viewModel': MaterialButton,
+		'viewModel': {
+			createViewModel: function(params, componentInfo) {
+				var vm = new MaterialButton(params);
+				var sub = ko.bindingEvent.subscribe(componentInfo.element, 'descendantsComplete', function(node) {
+					new materialRipple.MDCRipple($(node).find('.mdc-button')[0]);
+				});
+				vm.dispose = () => sub.dispose();
+				return vm;
+			}
+		},
 		'template': htmlString
 	};
 });
