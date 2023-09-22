ac7d89564e945ab24ef1728a6303e1c36d95d348
Martin Klemsa <m.klemsa@gmail.com>
2023-09-22 11:31:46 +0200

    Added material-icon-button-toggle

---
+++ material-icon-button-toggle/material-icon-button-toggle.js
@@ -0,0 +1,34 @@
+﻿define(['text!./material-icon-button-toggle.html', '../tools/tools', '@material/icon-button'],
+function(htmlString, tools, materialIconButton) {
+
+	var MaterialIconButtonToggle = function(params) {
+		this.value = params.value;
+		this.icons = params.icons;
+		this.titles = params.titles;
+		this.enable = tools.readEnableStatus(params);
+	};
+	MaterialIconButtonToggle.prototype = {
+		'toggle': function() {
+			this.value(!this.value());
+		},
+		'getCss': function() {
+			return {
+				'mdc-icon-button--on': this.value()
+			};
+		}
+	};
+
+	return {
+		'viewModel': {
+			createViewModel: function(params, componentInfo) {
+				var vm = new MaterialIconButtonToggle(params);
+				var sub = ko.bindingEvent.subscribe(componentInfo.element, 'descendantsComplete', function(node) {
+					new materialIconButton.MDCIconButtonToggle($(node).find('.mdc-icon-button')[0]);
+				});
+				vm.dispose = () => sub.dispose();
+				return vm;
+			}
+		},
+		'template': htmlString
+	};
+});
