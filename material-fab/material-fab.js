29a687a59811a414e6b91260d932a52e1eee5c4e
Martin Klemsa <m.klemsa@gmail.com>
2023-10-13 10:11:58 +0200

    Added material-fab

---
+++ material-fab/material-fab.js
@@ -0,0 +1,22 @@
+﻿define(['text!./material-fab.html', '../tools/tools', '@material/ripple'],
+function(htmlString, tools, materialRipple) {
+
+	var MaterialFab = function(params) {
+		this.click = params.click;
+		this.enable = tools.readEnableStatus(params);
+	};
+
+	return {
+		'viewModel': {
+			createViewModel: function(params, componentInfo) {
+				var vm = new MaterialFab(params);
+				const sub = ko.bindingEvent.subscribe(componentInfo.element, 'descendantsComplete', node => {
+					new materialRipple.MDCRipple($(node).find('.mdc-fab')[0]);
+				});
+				vm.dispose = () => sub.dispose();
+				return vm;
+			}
+		},
+		'template': htmlString
+	};
+});
