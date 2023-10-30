9aeaf3279016d0d8ed819f655fcc4fed4c8fd1a8
Martin Klemsa <m.klemsa@gmail.com>
2023-10-30 10:22:42 +0100

    Changed material-progress to use mdc-linear-progress

--- material-progress/material-progress.js
+++ material-progress/material-progress.js
@@ -1,17 +1,53 @@
-﻿define(['text!./material-progress.html', '../tools/tools'],
-function(htmlString, tools) {
+﻿define(['text!./material-progress.html', '@material/linear-progress'],
+function(htmlString, materialLinearProgress) {
 
 	var MaterialProgress = function(params) {
-		this.indeterminate = params.indeterminate;
-		this.min = params.min || 0;
-		this.max = params.max || 100;
-		this.secondaryProgress = params.secondaryProgress || 0;
+		this.label = params.label;
 		this.value = params.value;
-		this.enable = tools.readEnableStatus(params);
+		this.indeterminate = params.indeterminate;
+
+		//component lifetime
+		this.bindingSubscription = null;
+		this.mdcLinearProgress = null;
+		this.valueSubscription = null;
+	};
+	MaterialProgress.prototype = {
+		'dispose': function() {
+			if (this.valueSubscription)
+				this.valueSubscription.dispose();
+
+			this.mdcLinearProgress.destroy();
+			this.bindingSubscription.dispose();
+		},
+		'getAttrs': function() {
+			return {
+				'aria-label': this.label,
+				'aria-valuenow': this.value
+			};
+		},
+		'getCss': function() {
+			return {
+				'mdc-linear-progress--indeterminate': this.indeterminate,
+			};
+		}
 	};
 
 	return {
-		'viewModel': MaterialProgress,
+		'viewModel': {
+			'createViewModel': function(params, componentInfo) {
+				var vm = new MaterialProgress(params);
+				vm.bindingSubscription = ko.bindingEvent.subscribe(componentInfo.element, 'descendantsComplete', node => {
+					vm.mdcLinearProgress = new materialLinearProgress.MDCLinearProgress($(node).find('.mdc-linear-progress')[0]);
+					if (vm.value) {
+						vm.mdcLinearProgress.progress = ko.unwrap(vm.value);
+						vm.valueSubscription = vm.value.subscribe(newVal => {
+							vm.mdcLinearProgress.progress = newVal;
+						});
+					}
+				});
+				return vm;
+			}
+		},
 		'template': htmlString
 	};
 });
