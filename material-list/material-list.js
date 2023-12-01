3bef47f2b3d05eb41cc5a6f6f6dd781f9dc3f0da
Martin Klemsa <m.klemsa@gmail.com>
2023-12-01 11:46:26 +0100

    Added support for avatars and two-line items in list

--- material-list/material-list.js
+++ material-list/material-list.js
@@ -3,27 +3,51 @@ function(htmlString, materialList, materialRipple) {
 
 	const MaterialList = function(params) {
 		this.fast = params.fast; //prevent ripple effect
+		this.avatars = params.avatars;
 		this.icons = params.icons;
+		this.twoLines = params.twoLines;
 
 		//component lifetime
+		this.observer = null;
 		this.mdcList = null;
 		this.mdcRipples = [];
 	};
 	MaterialList.prototype = {
 		'koDescendantsComplete': function(node) {
-			this.mdcList = new materialList.MDCList($(node).find('.mdc-deprecated-list')[0]);
-			if (!this.fast)
-				this.mdcRipples = this.mdcList.listElements.map(listItemEl => new materialRipple.MDCRipple(listItemEl));
+			//mdc-list is often hidden behind a menu button,
+			//so it's better to defer initialization till it's visible
+			this.observer = new IntersectionObserver(entries => {
+				entries.forEach(entry => {
+					if (entry.isIntersecting) {
+						this._init(entry.target);
+						this.observer.disconnect();
+						this.observer = null;
+					}
+				});
+			});
+			this.observer.observe(node);
 		},
 		'dispose': function() {
+			if (this.observer)
+				this.observer.disconnect();
+
 			this.mdcRipples.forEach(ripple => ripple.destroy());
-			this.mdcList.destroy();
+			if (this.mdcList)
+				this.mdcList.destroy();
 		},
 
 		'getCss': function() {
 			return {
-				'mdc-deprecated-list--icon-list': this.icons
+				'mdc-deprecated-list--avatar-list': this.avatars,
+				'mdc-deprecated-list--icon-list': this.icons,
+				'mdc-deprecated-list--two-line': this.twoLines
 			};
+		},
+
+		'_init': function(node) {
+			this.mdcList = new materialList.MDCList($(node).find('.mdc-deprecated-list')[0]);
+			if (!this.fast)
+				this.mdcRipples = this.mdcList.listElements.map(listItemEl => new materialRipple.MDCRipple(listItemEl));
 		}
 	};
 
