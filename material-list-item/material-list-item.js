f2db04032e93a768315b441270a72355d41d8534
Martin Klemsa <m.klemsa@gmail.com>
2023-12-21 14:02:23 +0100

    Removed material-list-icon-item

--- material-list-item/material-list-item.js
+++ material-list-item/material-list-item.js
@@ -2,14 +2,16 @@
 function(htmlString, tools) {
 
 	const MaterialListItem = function(params) {
+		this.icon = params.icon;
+		this.text = params.text;
+		this.value = params.value;
+		this.enable = tools.readEnableStatus(params);
+
 		this.click = typeof params.click != 'function' ? params.click : function(vm, event) {
 			//the timeout means the parent list's selection handler will execute
 			//even if the clicked item gets disabled as a result of click processing
 			setTimeout(() => params.click(vm, event));
 		};
-		this.text = params.text;
-		this.value = params.value;
-		this.enable = tools.readEnableStatus(params);
 	};
 	MaterialListItem.prototype = {
 		'getCss': function() {
